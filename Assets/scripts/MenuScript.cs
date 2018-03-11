8c7a700b23ae8d0ee8b1b87e32399df14e078c85
Alexis CRAYE <alexis1.craye@epitech.eu>
2018-03-11 20:22:59 +0100

    initial commit

---
+++ Assets/scripts/MenuScript.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MenuScript : MonoBehaviour {
+    public void StartGame()
+    {
+        // "Stage1" is the name of the first scene we created.
+        SceneManager.LoadScene("Game");
+    }
+}
