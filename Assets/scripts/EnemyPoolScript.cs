731d5509198a3ccf779a26edffe901dc9929f9b2
Alexis CRAYE <alexis1.craye@epitech.eu>
2018-03-13 01:09:02 +0100

    ene,ypool

---
+++ Assets/scripts/EnemyPoolScript.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class EnemyPoolScript : MonoBehaviour {
+    public float spawnRate = 2.5f;
+    public List<Transform> AvailableObjects;
+
+    private float spawnCooldown;
+
+    void Start()
+    {
+        spawnCooldown = 0f;
+    }
+
+    void Update()
+    {
+        if (spawnCooldown > 0)
+            spawnCooldown -= Time.deltaTime;
+        if (spawnCooldown <= 0f)
+            GenerateEnemy(Random.Range(0, AvailableObjects.Count - 1));
+    }
+
+    public void GenerateEnemy(int index)
+    {
+        if (index < AvailableObjects.Count)
+        {
+            Instantiate(AvailableObjects[0]);
+            spawnCooldown = spawnRate;
+        }
+    }
+}
